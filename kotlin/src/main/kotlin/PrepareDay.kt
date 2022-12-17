cfbdeca3878cd544445e234ef5a146004b71ed67
lars.gustafson@gmail.com <lars.gustafson@gmail.com>
2022-12-17 15:24:43 +0100

    Adding generator for new days

---
+++ kotlin/src/main/kotlin/PrepareDay.kt
@@ -0,0 +1,50 @@
+import java.io.File
+import java.util.*
+
+fun main() {
+
+
+    val year = input("Year", Calendar.getInstance().get(Calendar.YEAR).toString())?.toInt()
+    val day = input("Day", Calendar.getInstance().get(Calendar.DAY_OF_MONTH).toString())?.toInt()
+
+    val src = File("src/main/kotlin")
+
+    val yearFolder = File(src, "year$year")
+    if (!yearFolder.exists()) yearFolder.mkdir()
+
+    val dayFolder = File(yearFolder, String.format("day%02d", day))
+    if (!dayFolder.exists()) dayFolder.mkdir()
+
+    val dayName = String.format("Day%02d", day)
+    val dayFile = File(dayFolder, "$dayName.kt")
+    if (dayFile.exists()) {
+        println("File already exists!")
+        return
+    }
+    println("Creating $dayFile")
+    val fileContent = """
+package ${yearFolder.name}.${dayFolder.name}
+
+import commons.Day
+
+fun main() = println($dayName())
+
+class $dayName : Day() {
+}      
+    """.trimIndent()
+
+    dayFile.writeText(fileContent)
+    println("Done!")
+}
+
+
+private fun input(question: String, default: String? = null): String? {
+    print(question)
+    default?.run {
+        print(" ($default)")
+    }
+    print(":")
+    Scanner(System.`in`).nextLine().apply {
+        return if (isEmpty()) default else this
+    }
+}
